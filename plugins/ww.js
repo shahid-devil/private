244bb0a51080a7552a64fb325a0d7d6a83f88a5a
RavinduManoj <89992202+RavinduManoj@users.noreply.github.com>
2021-09-07 16:36:57 +0530

    😒

--- plugins/ww.js
+++ plugins/ww.js
@@ -1,4 +1,4 @@
-/*
+
 const QueenSew = require("../events");
 const { MessageType } = require("@adiwajshing/baileys");
 const { UploadToImgur } = require("../Utilis/Misc");
@@ -17,4 +17,4 @@ QueenSew.newcmdaddtosew({ pattern: "url", fromMe: true, desc: "Image to url." },
     return await message.sendMessage(url, { quoted: message.data });
   }
 );
-*/
+
