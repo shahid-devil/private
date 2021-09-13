cace146cbb5ad7b335d55d394c3e34dcbfad053a
RavinduManoj <89992202+RavinduManoj@users.noreply.github.com>
2021-09-13 11:43:21 +0530

    Update emaker.js

--- plugins/emaker.js
+++ plugins/emaker.js
@@ -169,12 +169,14 @@ QueenSew.newcmdaddtosew({pattern: 'sl3space ?(.*)', fromMe: sk, dontAdCommandLis
     var buffer_data = await axios.get(img.image, { responseType: 'arraybuffer'})
     await message.sendMessage(Buffer.from(buffer_data.data), MessageType.image, { caption: Raviya.CPK })
 }));
+  /*
 QueenSew.newcmdaddtosew({pattern: 'slangle ?(.*)', fromMe: sk, dontAdCommandList: true}, (async (message, match) => {
     var img = await Pach.rgmsd(match[1], 'https://en.ephoto360.com/wings-galaxy-206.html')
    var AQRS = await Pach.Sewbase('https://i.ibb.co/MswXM2W/bf3808856cd9.jpg')
     var buffer_data = await axios.get(img.image, { responseType: 'arraybuffer'})
     await message.sendMessage(Buffer.from(buffer_data.data), MessageType.image, { caption: Raviya.CPK, thumbnail: '${AQRS.data}')
 }));
+*/
 QueenSew.newcmdaddtosew({pattern: 'sl2pink ?(.*)', fromMe: sk, dontAdCommandList: true}, (async (message, match) => {
     var img = await Pach.rgmsd(match[1], 'https://en.ephoto360.com/create-a-blackpink-neon-logo-text-effect-online-710.html')
     var buffer_data = await axios.get(img.image, { responseType: 'arraybuffer'})
